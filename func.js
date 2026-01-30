0c0e4f3d39549246f8aaa8d7ba9f15138ba57a23
Emmanuel BONIN <emmanuel@bodyguard.ai>
2026-01-30 13:44:56 +0000

    fix: buttons working again after github refactoring of the PR page + added style

--- func.js
+++ func.js
@@ -1,37 +1,26 @@
 window.addEventListener('load', () => {
+  chrome.tabs.query({ active: true, currentWindow: true, url: 'https://github.com/*/pull/*/changes' }, (tabs) => {
+    if (tabs.length) {
+      document.getElementById('tooltip').setAttribute('hidden', 'true')
+      document.getElementById('container').removeAttribute('hidden')
+    }
+  })
+
   document.getElementById('view-all').addEventListener('click', async () => {
-    chrome.tabs.query({}, result => {
-      result.map(tab => {
-        if (/^https:\/\/github\.com\/*/.test(tab.url)) {
-          chrome.tabs.sendMessage(tab.id, 'viewAll', res => {
-            console.log('res', res)
-          })
-        }
-      })
+    chrome.tabs.query({ active: true, currentWindow: true, url: 'https://github.com/*/pull/*/changes' }, (tabs) => {
+      chrome.tabs.sendMessage(tabs[0].id, 'viewAll')
     })
   })
 
   document.getElementById('unview-all').addEventListener('click', async () => {
-    chrome.tabs.query({}, result => {
-      result.map(tab => {
-        if (/^https:\/\/github\.com\/*/.test(tab.url)) {
-          chrome.tabs.sendMessage(tab.id, "unViewAll", (response) => {
-            console.log(response)
-          })
-        }
-      })
+    chrome.tabs.query({ active: true, currentWindow: true, url: 'https://github.com/*/pull/*/changes' }, (tabs) => {
+      chrome.tabs.sendMessage(tabs[0].id, 'unViewAll')
     })
   })
 
   document.getElementById('switch-all').addEventListener('click', async () => {
-    chrome.tabs.query({}, result => {
-      result.map(tab => {
-        if (/^https:\/\/github\.com\/*/.test(tab.url)) {
-          chrome.tabs.sendMessage(tab.id, "switchAll", (response) => {
-            console.log(response)
-          })
-        }
-      })
+    chrome.tabs.query({ active: true, currentWindow: true, url: 'https://github.com/*/pull/*/changes' }, (tabs) => {
+      chrome.tabs.sendMessage(tabs[0].id, 'switchAll')
     })
   })
 })
